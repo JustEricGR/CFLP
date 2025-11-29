84162a63a79f27c4957e72870a17adefdb816966
JustEricGR <eric.ratiu@gmail.com>
2025-11-29 14:46:20 +0200

    incarcare proiect

--- lab7/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ lab7/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6906c05cb068c7a83188a9a65c13a8bff6e7bbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ceb8335bce32578c7713dbff8f712631eb4584")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
